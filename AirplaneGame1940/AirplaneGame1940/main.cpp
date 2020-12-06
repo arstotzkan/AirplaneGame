6215af86aecde0bae3d475f0b8b67a15195e190b
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2020-12-06 13:34:53 +0200

    Update main.cpp

--- AirplaneGame1940/AirplaneGame1940/main.cpp
+++ AirplaneGame1940/AirplaneGame1940/main.cpp
@@ -14,13 +14,55 @@
 *		-enemy (maybe)
 */
 
+void update(float ms)
+{
+    graphics::MouseState mouse;
+    graphics::getMouseState(mouse);
+    if (mouse.button_left_released)
+    {
+        graphics::playSound("assets\\door2.wav", 1.0f, false);
+    }
+}
+
+// The window content drawing function.
+void draw()
+{
+    graphics::Brush br;
+
+    graphics::setOrientation(45);
+
+    br.texture = "assets\\boy2.png";
+    br.outline_opacity = 0.0f;
+
+    graphics::drawRect(500, 250, 200, 200, br);
+
+    br.fill_color[0] = 0.5f;
+    br.fill_color[1] = 0.0f;
+    br.fill_color[2] = 0.0f;
+
+    graphics::resetPose();
+    drawText(300, 440, 70, "Hello World!", br);
+}
+
 int main()
 {
-    int x = 3;
-    std::cout << 2;
+    graphics::createWindow(1200, 600, "Hello World");
 
-    //placeholder
-    //peepeepoopoo
-    //hahahahhaha
-    //ahaha
+    graphics::setDrawFunction(draw);
+    graphics::setUpdateFunction(update);
+
+    graphics::setCanvasSize(1000, 500);
+    graphics::setCanvasScaleMode(graphics::CANVAS_SCALE_FIT);
+
+    graphics::Brush br;
+    br.fill_color[0] = 0.5f;
+    br.fill_color[1] = 0.7f;
+    br.fill_color[2] = 0.9f;
+    graphics::setWindowBackground(br);
+
+    graphics::setFont("assets\\orange juice 2.0.ttf");
+
+    graphics::startMessageLoop();
+
+    return 0;
 }
