2162639b347952a1fd5e385c304b5dbeb49c832a
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2020-12-06 14:33:32 +0200

    cklass

--- AirplaneGame1940/AirplaneGame1940/Entity.h
+++ AirplaneGame1940/AirplaneGame1940/Entity.h
@@ -1,2 +1,16 @@
 #pragma once
+#include "graphics.h"
 
+class Entity
+{
+private:
+	float x;
+	float y;
+	float width;
+	float height;
+
+	Entity(float x , float y, float width , float height)
+	void drawAndUpdate();
+
+
+};
