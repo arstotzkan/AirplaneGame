f5436ab326938e4d3d09b1a830277a0d98c5c265
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2020-12-06 14:46:27 +0200

    Update Entity.cpp

--- AirplaneGame1940/AirplaneGame1940/Entity.cpp
+++ AirplaneGame1940/AirplaneGame1940/Entity.cpp
@@ -9,5 +9,15 @@ Entity::Entity(float temp_x, float temp_y, float temp_width, float temp_height)
 	width = temp_width;
 	height = temp_height;
 }
-Entity();
-void drawAndUpdate();
+Entity::Entity()
+{
+	x = 400;
+	y = 400;
+	width = 100;
+	height = 50;
+}
+
+Entity::drawAndUpdate()
+{
+
+}
