14444c7360956a40f04ad624cafacff9a8e23d8c
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2020-12-06 02:45:05 +0200

    comment

--- AirplaneGame1940/AirplaneGame1940/main.cpp
+++ AirplaneGame1940/AirplaneGame1940/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+/*class hierarchy:
+* -item
+*	-airplane
+*		-player
+*		-enemy
+*			-fighter
+*			-bomber
+*			-heavy bomber
+*	-projectile 
+*		-player(maybe)
+*		-enemy (maybe)
+*/
 int main()
 {
 
