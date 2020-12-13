06c965a30cf9535c20637e402fa9e4a725810d4d
Christos Moutaftsis <chrismoutaftsis@gmail.com>
2020-12-13 15:44:33 +0200

    Create Factory.cpp

---
+++ AirplaneGame1940/AirplaneGame1940/Factory.cpp
@@ -0,0 +1,20 @@
+#include "Factory.h"
+#include "graphics.h"
+#include "entity.h"
+
+Factory::Factory() : EnemyPlane(lvl)
+{
+
+}
+
+void Factory::draw()
+{
+
+}
+
+void Factory::update()
+{
+
+}
+
+
