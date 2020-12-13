93fa7c2248ebaa3ffb2f8aa987ff55f7c9c2911d
Christos Moutaftsis <chrismoutaftsis@gmail.com>
2020-12-13 14:11:56 +0200

    Update enemyplane.cpp

--- AirplaneGame1940/AirplaneGame1940/enemyplane.cpp
+++ AirplaneGame1940/AirplaneGame1940/enemyplane.cpp
@@ -8,17 +8,29 @@
 
 EnemyPlane::EnemyPlane(int lvl) : Airplane(lvl)
 {
-
+	healthPoints = 2;
 }
 
 void EnemyPlane::draw()
 {
+	graphics::Brush br;
 
+	graphics::setOrientation(0);
+	br.outline_opacity = 0.0f;
 }
 
 void EnemyPlane::update(std::list <Projectile>& projectileList)
 {
+	if (graphics::getKeyState(graphics::SCANCODE_W) || graphics::getKeyState(graphics::SCANCODE_UP))
+		y -= 5;
+	if (graphics::getKeyState(graphics::SCANCODE_S) || graphics::getKeyState(graphics::SCANCODE_DOWN))
+		y += 5;
+	if (graphics::getKeyState(graphics::SCANCODE_A) || graphics::getKeyState(graphics::SCANCODE_LEFT))
+		x -= 5;
+	if (graphics::getKeyState(graphics::SCANCODE_D) || graphics::getKeyState(graphics::SCANCODE_RIGHT))
+		x += 5;
 
+	borderCheck();
 }
 
 bool EnemyPlane::borderCheck()
