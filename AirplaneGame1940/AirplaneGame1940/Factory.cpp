d6923951a3a3935384c6d82e25d501c621075ad3
Panagiotis <35231321+arstotzkan@users.noreply.github.com>
2020-12-13 20:58:04 +0200

    Update Factory.cpp

--- AirplaneGame1940/AirplaneGame1940/Factory.cpp
+++ AirplaneGame1940/AirplaneGame1940/Factory.cpp
@@ -13,7 +13,7 @@ Factory::Factory()
 
 void Factory::update(std::list <EnemyPlane> &enemyList)
 {
-	if (counter % 10000 == 1500)
+	if (counter % 10000 == 5/*1000*/)
 	{
 		EnemyPlane* a1 = new EnemyPlane(1, 100, 100);
 		EnemyPlane* a2 = new EnemyPlane(1, 250, 100);
