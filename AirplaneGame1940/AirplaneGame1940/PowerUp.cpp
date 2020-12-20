c082cc937aeb51588f7d03b0307b5a6eeae500db
Christos Moutaftsis <chrismoutaftsis@gmail.com>
2020-12-20 13:59:56 +0200

    Create PowerUp.cpp

---
+++ AirplaneGame1940/AirplaneGame1940/PowerUp.cpp
@@ -0,0 +1,24 @@
+#include "PowerUp.h"
+
+PowerUp::PowerUp(float vel)
+{
+}
+PowerUp::PowerUp()
+{
+	velocity = 2;
+}
+PowerUp::PowerUp(float vel)
+{
+	velocity = vel;
+}
+void PowerUp::draw()
+{
+
+}
+void PowerUp::update()
+{
+}
+bool PowerUp::borderCheck()
+{
+	return false;
+}
