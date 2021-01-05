db40ee40826ffb5fe7b0d5d0988235eeebd42403
Christos Moutaftsis <chrismoutaftsis@gmail.com>
2021-01-05 14:47:09 +0200

    Update PowerUp.cpp

--- AirplaneGame1940/AirplaneGame1940/PowerUp.cpp
+++ AirplaneGame1940/AirplaneGame1940/PowerUp.cpp
@@ -4,7 +4,7 @@ PowerUp::PowerUp(float t_x, float t_y) : Entity::Entity(t_x , t_y, 15 , 40 , 40)
 {
 }
 
-void PowerUp::draw()
+void PowerUp::draw() // zwgrafizei to power up sthn othoni
 {
 	graphics::Brush br;
 
@@ -15,11 +15,11 @@ void PowerUp::draw()
 	br.texture = "assets/powerup.png";
 	graphics::drawRect(x, y, width, height, br);
 }
-void PowerUp::update()
+void PowerUp::update() // anaparagei to power up
 {
 	y += velocity * graphics::getDeltaTime() / 333;
 }
-bool PowerUp::borderCheck()
+bool PowerUp::borderCheck() // kanei elegxo wste na mhn bgei apo ta perithoria ths othonis
 {
 	if (y > 900)
 		return true;
